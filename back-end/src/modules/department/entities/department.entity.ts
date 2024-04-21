db589f07a371b5d63f264e73968a3d8cd32accd7
Amanuel Sisay <amanuelsisay.as@gmail.com>
2024-04-21 21:32:41 +0300

    (feat): script to populate data inot database

--- back-end/src/modules/department/entities/department.entity.ts
+++ back-end/src/modules/department/entities/department.entity.ts
@@ -2,9 +2,9 @@ import { Entity, Column } from 'typeorm';
 import { ApiProperty } from '@nestjs/swagger';
 import { BaseModelEntity } from 'src/modules/BaseEntity/base-model.entity';
 
-@Entity()
+@Entity('departments')
 export class Department extends BaseModelEntity {
-  @Column()
+  @Column({unique: true})
   @ApiProperty({ description: 'The name of the department.' })
   name: string;
 }
--- back-end/src/modules/role/entities/role.entity.ts
+++ back-end/src/modules/role/entities/role.entity.ts
@@ -3,7 +3,7 @@ import { ApiProperty } from '@nestjs/swagger';
 import { BaseModelEntity } from 'src/modules/BaseEntity/base-model.entity';
 import { UserRoleEnum } from 'src/modules/user/entities/user-role.enum';
 
-@Entity()
+@Entity('roles')
 export class Role extends BaseModelEntity {
   @Column({ unique: true })
   @ApiProperty({ description: 'Name of the role', enum: UserRoleEnum, default: 'User' })
