0bcf4747fe756953d4574f03ad56f8ac858930e0
Amanuel Sisay <amanuelsisay.as@gmail.com>
2024-05-15 09:47:03 +0300

    add global prefix

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -6,6 +6,7 @@ import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
   dotenv.config();
 
