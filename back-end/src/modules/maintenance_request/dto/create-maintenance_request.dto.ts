e3e0ef5ac5ad3c6fabdac2a6043d1e20a155f9dd
Amanuel Sisay <amanuelsisay.as@gmail.com>
2024-05-19 17:23:53 +0300

    (feat): add examples to the documentation

--- back-end/src/modules/maintenance_request/dto/create-maintenance_request.dto.ts
+++ back-end/src/modules/maintenance_request/dto/create-maintenance_request.dto.ts
@@ -1,30 +1,44 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsEnum, IsOptional, IsNumber, Min, Max, IsString, IsDate, IsArray, IsInt } from 'class-validator';
-import { MaintenanceVerificationStatusEnum, MaintenanceConfirmationStatusEnum } from '../entities/maintenance_request.enum';
+import { IsNotEmpty, IsOptional, IsString, IsArray, IsInt } from 'class-validator';
 import { CreateLocationDto } from 'src/modules/location/dto/create-location.dto';
 
 export class CreateMaintenanceRequestDto {
-  @ApiProperty({ description: 'The subject of the maintenance request' })
+  @ApiProperty({
+    description: 'The subject of the maintenance request',
+    example: 'Leaking Pipe in Bathroom',
+  })
   @IsNotEmpty()
+  @IsString()
   subject: string;
 
-  @ApiProperty({ description: 'The description of the maintenance request (Markdown)' })
+  @ApiProperty({
+    description: 'The description of the maintenance request (Markdown)',
+    example: 'There is a leaking pipe in the bathroom that needs urgent repair.',
+  })
   @IsNotEmpty()
   @IsString()
   description: string;
 
-  @ApiProperty({ description: 'The location ID of the maintenance request' })
+  @ApiProperty({
+    description: 'The location of the maintenance request',
+    type: CreateLocationDto,
+  })
   @IsNotEmpty()
-  @IsNumber()
   locationCreate: CreateLocationDto;
 
-  @ApiProperty({ description: 'The IDs of maintenance request types associated with this request' })
+  @ApiProperty({
+    description: 'The IDs of maintenance request types associated with this request',
+    example: [1],
+  })
   @IsOptional()
   @IsArray()
   @IsInt({ each: true })
   maintenanceRequestTypeIds: number[];
 
-  @ApiProperty({ description: 'The IDs of media files associated with this maintenance request' })
+  @ApiProperty({
+    description: 'The IDs of media files associated with this maintenance request',
+    example: [1],
+  })
   @IsOptional()
   @IsArray()
   @IsInt({ each: true })
@@ -32,7 +46,7 @@ export class CreateMaintenanceRequestDto {
 
   @ApiProperty({
     description: 'The priority of the request, by default it is -1',
-    example: -1,
+    example: 0,
     default: -1,
   })
   @IsInt()
