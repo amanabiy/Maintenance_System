6e8be3329c2556020c6c49d7cb12e1ca20f76d87
Amanuel K <amanuelabebe2000@gmail.com>
2024-05-26 01:54:27 +0300

    feat:ImageSlider setup feat:Request Detail basics without map done feat:minor changes to ui

---
+++ front-end/src/components/commonScenes/RequestDetails.jsx
@@ -0,0 +1,149 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+import {
+  Button,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  Avatar,
+  IconButton,
+  Tooltip,
+} from "@mui/material";
+import GridParent from "../layout/GridParent";
+import GridItem from "../layout/GridItem";
+import LeakingPipe1 from "../../assets/images/leakingPipe.jpg";
+import LeakingPipe2 from "../../assets/images/leaking-pipe2.webp";
+import ImageSlider from "../display/ImageSlider";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+// import Map from "../../components/Map"; // Assuming you have a Map component for displaying the location
+
+const RequestDetails = () => {
+  const { requestId } = useParams();
+
+  const maintenanceRequest = {
+    createdAt: "2024-05-25T17:45:16.295Z",
+    updatedAt: "2024-05-25T17:45:16.295Z",
+    subject: "Leaking Pipe",
+    description:
+      "There is a leak in the pipe in the restroom on the fifth floor.",
+    location: {
+      createdAt: "2024-05-25T17:45:16.295Z",
+      updatedAt: "2024-05-25T17:45:16.295Z",
+      blockNumber: "B57",
+      floor: 5,
+      latitude: 34.0522,
+      longitude: -118.2437,
+      roomNumber: "204",
+      isToilet: true,
+    },
+    verificationStatus: "PASSED",
+    verifiedBy: {
+      name: "John Doe",
+      role: "Maintenance Supervisor",
+    },
+    verifiedAt: "2024-05-25T17:45:16.295Z",
+    confirmationStatus: "NOT_COMPLETED",
+    rating: 4,
+    priority: 1,
+    feedback: "Fixed in a timely manner.",
+    requester: {
+      name: "Jane Smith",
+      contact: "jane.smith@example.com",
+    },
+    assignedPersons: ["Mike Johnson", "Emily Davis"],
+    maintenanceRequestTypes: ["Plumbing", "Urgent"],
+    handlingDepartment: {
+      createdAt: "2024-05-25T17:45:16.295Z",
+      updatedAt: "2024-05-25T17:45:16.295Z",
+      name: "Facilities Management",
+    },
+    mediaFiles: ["leak_photo1.jpg", "leak_photo2.jpg"],
+    requestStatuses: ["Pending", "In Progress", "Completed"],
+  };
+
+  return (
+    <div style={{ padding: "16px" }}>
+      <GridParent>
+        <GridItem xs={12} md={6}>
+          <Card
+            style={{
+              maxWidth: "1200px",
+              width: "100%",
+              aspectRatio: "10 / 6",
+              margin: "0 auto",
+              borderRadius: "8px",
+            }}
+          >
+            <ImageSlider images={[LeakingPipe1, LeakingPipe2]} />
+          </Card>
+          <CardContent
+            style={{
+              padding: "16px",
+              display: "flex",
+              flexDirection: "column",
+              gap: "5px",
+            }}
+          >
+            <Typography variant="h3">
+              Subject: <u>{maintenanceRequest.subject}</u>
+            </Typography>
+            <Typography variant="body1" style={{ margin: "8px 0" }}>
+              Provided Description:{" "}
+              <Typography variant="body2">
+                {maintenanceRequest.description}
+              </Typography>
+            </Typography>
+            <Typography variant="body1">
+              Location:
+              <Typography variant="body2">
+                <strong>Block: </strong>
+                {maintenanceRequest.location.blockNumber},{" "}
+              </Typography>
+              <Typography variant="body2">
+                <strong>Room Number: </strong>
+                {maintenanceRequest.location.roomNumber},{" "}
+              </Typography>
+              <Typography variant="body2">
+                <strong>Floor: </strong>
+                {maintenanceRequest.location.floor}
+              </Typography>
+            </Typography>
+            <div>
+              <Typography variant="body1">Requester:</Typography>
+              <Card
+                style={{
+                  width: "fit-content",
+                  padding: "5px",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "8px",
+                }}
+              >
+                <Avatar style={{ width: "24px", height: "24px" }} />
+                <Typography variant="body2">
+                  {maintenanceRequest.requester.name}
+                </Typography>
+                <Tooltip title="View Requester Profile">
+                  <IconButton>
+                    <OpenInNewIcon />
+                  </IconButton>
+                </Tooltip>
+              </Card>
+            </div>
+          </CardContent>
+        </GridItem>
+        <GridItem xs={12} md={6}>
+          <div style={{ aspectRatio: "1/1", border: "1px solid black" }}>
+            {/* <Map
+              latitude={maintenanceRequest.location.latitude}
+              longitude={maintenanceRequest.location.longitude}
+            /> */}
+          </div>
+        </GridItem>
+      </GridParent>
+    </div>
+  );
+};
+
+export default RequestDetails;
--- front-end/src/pages/Admin/Requests.jsx
+++ front-end/src/pages/Admin/Requests.jsx
@@ -1,7 +1,136 @@
 import React from "react";
+import GridParent from "../../components/layout/GridParent";
+import GridItem from "../../components/layout/GridItem";
+import DataTable from "../../components/tables/DataTable";
+import { mockRequestData } from "../../data/mockRequestData";
+import { useTheme } from "@mui/material/styles";
+import { tokens } from "../../theme";
+import { IconButton, Tooltip } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import EditIcon from "@mui/icons-material/Edit";
+import CheckIcon from "@mui/icons-material/Check";
+import BuildIcon from "@mui/icons-material/Build";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
 
 const Requests = () => {
-  return <div>Requests</div>;
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const navigate = useNavigate();
+  const columns = [
+    { field: "id", headerName: "ID" },
+    { field: "createdAt", headerName: "Request Date", flex: 1 },
+    { field: "Subject", headerName: "Subject", flex: 1 },
+    {
+      field: "verificationStatus",
+      headerName: "Verification Status",
+      renderCell: (params) =>
+        params.value ? (
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              style={{
+                width: "90px",
+                height: "30px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+                borderRadius: "5px",
+                backgroundColor: colors.greenAccent[500],
+              }}
+            >
+              <DoneAllIcon /> Verified
+            </div>
+          </div>
+        ) : (
+          <div style={{ color: colors.redAccent[500] }}>Not Verified </div>
+        ),
+    },
+    {
+      field: "confirmationStatus",
+      headerName: "Confirmation Status",
+      renderCell: (params) =>
+        params.value ? (
+          <div
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+          >
+            <div
+              style={{
+                width: "100px",
+                height: "30px",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "space-around",
+                borderRadius: "5px",
+                backgroundColor: colors.blueAccent[800],
+              }}
+            >
+              <BuildIcon /> Confirmed
+            </div>
+          </div>
+        ) : (
+          "Not Confirmed"
+        ),
+    },
+    {
+      field: "blockNumber",
+      headerName: "Block Number",
+      renderCell: (params) => <div>B-{params.value}</div>,
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      renderCell: (params) => {
+        return (
+          <div>
+            <Tooltip title="View Request">
+              <IconButton
+                onClick={(event) => {
+                  event.stopPropagation();
+                  navigate(`view/${params.id}`);
+                  // Add your edit user logic here
+                }}
+              >
+                <VisibilityIcon />
+              </IconButton>
+            </Tooltip>
+            <Tooltip title="Edit Request">
+              <IconButton
+                onClick={(event) => {
+                  event.stopPropagation();
+                  navigate(`edit/${params.id}`);
+                  // Add your edit user logic here
+                }}
+              >
+                <EditIcon />
+              </IconButton>
+            </Tooltip>
+          </div>
+        );
+      },
+    },
+  ];
+  return (
+    <GridParent>
+      <GridItem xs={12}>
+        <DataTable rows={mockRequestData} columns={columns} />
+      </GridItem>
+    </GridParent>
+  );
 };
 
 export default Requests;
