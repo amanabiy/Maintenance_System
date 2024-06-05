c72ca6352948108c8f70af318c71329bd477257d
Amanuel K <amanuelabebe2000@gmail.com>
2024-06-05 12:47:32 +0300

    minor change to conditional baseUrl assignment

--- front-end/src/redux/services/baseApi.js
+++ front-end/src/redux/services/baseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 const REACT_APP_LOCAL_API_URL = "http://23.94.117.101:8081/api/v1/";
-const REACT_APP_PROD_API_URL = "https://www.maintains.live/api";
+const REACT_APP_PROD_API_URL = "https://www.maintains.live/api/v1";
 const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
