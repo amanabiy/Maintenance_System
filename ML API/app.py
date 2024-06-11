bf41bf7a86fc47cadb82771905f1dbc4474ca54a
amanuelalehegne <amanuel.alehegne@a2sv.org>
2024-06-11 06:29:26 +0300

    Cors

--- ML API/app.py
+++ ML API/app.py
@@ -3,8 +3,11 @@ from flask_restx import Api, Resource, fields
 import pandas as pd
 import joblib
 import numpy as np
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
+
 api = Api(app, version='1.0', title='Maintenance Requests API',
           description='API to predict maintenance requests and estimate service time')
 
